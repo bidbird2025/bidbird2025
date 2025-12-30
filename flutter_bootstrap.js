dfad0b43aad25a24270b0d4fdc324bf3199cca2f
X <x@Xui-Macmini.local>
2025-12-31 03:04:45 +0900

    test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "567437406"
+    serviceWorkerVersion: "2967224164"
   }
 });
